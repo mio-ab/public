4695c29ec7b7a2bc54e7e35cc8676ad1ea6e9502
Mio AB <webtech@mio.se>
2014-01-24 16:18:50 +0100

    Update highlight.js
    Updated code indentation and documentation.

--- highlight.js
+++ highlight.js
@@ -1,32 +1,32 @@
 /**
- * Highlight part(s) of a text string
+ * Highlight parts of a text string
  *
- * @param {string} haystack Full text to be searched
- * @param {string} needle Text fragment to highlight in haystack
- * @return {string} Highlighted text
+ * @param {string} haystack The string being searched, otherwise known as the haystack.
+ * @param {string} needle The value being searched for, otherwise known as the needle.
+ * @return {string} This function returns a string with highlighted text.
  */
 function highlight( haystack, needle )
 {
-  var startTag  = '<span class="hl">',
-  closeTag      = '</span>',
-  highlight     = '',
-  i             = -1;
-     
-  while ( haystack.length > 0 )
-  {
-    i = haystack.toLowerCase().indexOf( needle.toLowerCase(), i+1 );
-    if ( i < 0 )
-    {
-      highlight   += haystack;
-      haystack = '';
-    }
-    else
-    {
-      highlight   += haystack.substring( 0, i ) + startTag + haystack.substr( i, needle.length ) + closeTag;
-      haystack = haystack.substr( i+needle.length );
-      i = -1;
-    }
-  }
-  
-  return highlight;
+	var openTag = '<span class="hl">';
+	var closeTag = '</span>';
+	var highlighted = '';
+	var i = -1;
+
+	while ( haystack.length > 0 )
+	{
+		i = haystack.toLowerCase().indexOf( needle.toLowerCase(), i+1 );
+		if ( i < 0 )
+		{
+			highlighted += haystack;
+			haystack = '';
+		}
+		else
+		{
+			highlighted += haystack.substring( 0, i ) + openTag + haystack.substr( i, needle.length ) + closeTag;
+			haystack = haystack.substr( i+needle.length );
+			i = -1;
+		}
+	}
+	
+	return highlighted;
 }

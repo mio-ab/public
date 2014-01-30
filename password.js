0dcf009d5cbc89298b9df106e9caff2c597e96c0
Mio AB <webtech@mio.se>
2014-01-30 15:48:50 +0100

    Updated indentation and documentation

--- password.js
+++ password.js
@@ -20,30 +20,30 @@ function password(lc, uc, num, pun, len) {
         SetsUsed = true;
 
     // Check that the value for parameter len is a number. If it's not, set len to zero (0).
-    if (typeof len !== 'number') var len = 0;
+    if ( typeof len !== 'number' ) var len = 0;
 
     // Check if the set with lower case letters is required 
-    if (lc !== false) Chars.push({
-        "Set": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-        "Used": false
+    if ( lc !== false ) Chars.push({
+        "Set" : ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+        "Used" : false
     });
 
     // Check if the set with upper case letters is required 
-    if (uc !== false) Chars.push({
-        "Set": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-        "Used": false
+    if ( uc !== false ) Chars.push({
+        "Set" : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+        "Used" : false
     });
 
     // Check if the set with numbers is required 
-    if (num !== false) Chars.push({
-        "Set": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-        "Used": false
+    if ( num !== false ) Chars.push({
+        "Set" : ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        "Used" : false
     });
 
     // Check if the set with punctuations is required 
-    if (pun !== false) Chars.push({
-        "Set": ["!", "@", "#", "%", "&", "(", ")", "{", "}", "[", "]", "=", "?", "+", "*", "^", "~", "-", "_", ".", ":", ",", ";", "'", "´", "`", "\"", "\\", "/", "$", "£", "€"],
-        "Used": false
+    if ( pun !== false ) Chars.push({
+        "Set" : ["!", "@", "#", "%", "&", "(", ")", "{", "}", "[", "]", "=", "?", "+", "*", "^", "~", "-", "_", ".", ":", ",", ";", "'", "´", "`", "\"", "\\", "/", "$", "£", "€"],
+        "Used" : false
     });
 
     // Make sure that len value is at least the same as the number of character sets to be used.
@@ -52,20 +52,23 @@ function password(lc, uc, num, pun, len) {
     // Start the loop
     for (var i = 0; i < len; i++) {
         // Get a random character set
-        var Set = Math.floor(Math.random() * Chars.length);
+        var Set = Math.floor( Math.random() * Chars.length );
+        
         // Get the position for a random character in the current set
-        var Pos = Math.floor(Math.random() * Chars[Set].Set.length);
+        var Pos = Math.floor( Math.random() * Chars[Set].Set.length );
+        
         // Append the found character to the output string
         Output += Chars[Set].Set[Pos];
+        
         // Mark the current set as used
         Chars[Set].Used = true;
     }
 
     // Check if all required sets were used
-    for (var i = 0; i < Chars.length; i++) if (SetsUsed == true && Chars[i].Used == false) SetsUsed = false;
+    for ( var i = 0; i < Chars.length; i++ ) if ( SetsUsed == true && Chars[i].Used == false ) SetsUsed = false;
 
     // If all required sets were NOT used, recursively request a new string to be generated
-    if (SetsUsed == false) return password(lc, uc, num, pun, len);
+    if ( SetsUsed == false ) return password( lc, uc, num, pun, len );
     
     // Return generated string
     else return Output;

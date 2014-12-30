ab3651ed3ab5b20bc521f0625462ba3a43ce0f51
Mio AB <webtech@mio.se>
2014-12-30 08:50:34 +0100

    Update randomString.js

--- randomString.js
+++ randomString.js
@@ -1,6 +1,10 @@
-function randomString()
-{
-  var s = "", c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for( var i=0; i < 7; i++ ) s += c.charAt(Math.floor(Math.random() * c.length));
-  return s;
+/* randstr - Generate a pseudo random string
+ * 
+ * @param {int} Output string length
+ * @returns {string} A pseudo random string
+ */
+function randstr( l ) {
+    var s = "", c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i=0; i < l; i++) s += c.charAt(Math.floor(Math.random()*c.length));
+    return s;
 }

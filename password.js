ae4b84ee34750a8fec9c2cc423b23352b7d49789
Mio AB <webtech@mio.se>
2016-11-22 17:07:25 +0100

    Update password.js

--- password.js
+++ password.js
@@ -4,39 +4,24 @@
  * @param int Length The length of the password
  * @return string The password
  */
-
-function generate_password( Length ) {
-	var Chars = [], Output = '', SetsUsed = true;
-
-	// Check that the value for parameter Length is a number and at least 4
-	// if it's not, set Length to 20 (default)
-	if ( typeof Length !== 'number' || Length < 4 ) Length = 20;
-
-	// Check if the set with lower case letters is required 
-	Chars.push({Set : 'abcdefghijklmnopqrstuvwxyz'.split(''), Used : false});
-	Chars.push({Set : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''), Used : false});
-	Chars.push({Set : '0123456789'.split(''), Used : false });
-	Chars.push({Set : '!@#%&(){}[]<>=?+*^~-_.:,;/|$£€\\\'`"'.split(''), Used : false });
-
-	for ( var i = 0; i < Length; i++ ) {
-		// Get a random character set
-		var Set = Math.floor( Math.random() * Chars.length );
-		// Get the position for a random character in the current set
-		var Pos = Math.floor( Math.random() * Chars[Set].Set.length );
-		// Append the found character to the output string
-		Output += Chars[Set].Set[Pos];
-		// Mark the current set as used
-		Chars[Set].Used = true;
-	}
-
-	// Check if all sets were used
-	for ( var i in Chars ) {
-		if ( SetsUsed === true && Chars[i].Used === false ) {
-			SetsUsed = false;
-		}
-	}
-
-	// If all sets were NOT used, recursively generated a new password,
-	// otherwise output the password
-	return (SetsUsed === false ? generate_password( Length ) : Output);
+function generate_pwd( len ) {
+    var i, set, pos, output = '', setsUsed = true, charSets = [
+        {set:'abcdefghijklmnopqrstuvwxyz'.split(''), used:false},
+        {set:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''), used:false},
+        {set:'0123456789'.split(''), used:false},
+        {set:'!@#%&(){}[]=?+*^~-_.:,;/|'.split(''), used:false}
+    ];
+    len = typeof len !== 'number' || len < 4 ? 20 : len;
+    for (i = 0; i < len; i++) {
+        set = Math.floor(Math.random()*charSets.length);
+        pos = Math.floor(Math.random()*charSets[set].set.length);
+        output += charSets[set].set[pos];
+        charSets[set].used = true;
+    }
+    for (i in charSets) {
+        if (setsUsed === true && charSets[i].used === false) {
+            setsUsed = false;
+        }
+    }
+    return setsUsed === false ? this(len) : output;
 }
